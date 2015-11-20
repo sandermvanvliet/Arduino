914a0163114deba1ceed2c821c584946c07c578d
sandermvanvliet <sander@codenizer.nl>
2015-11-20 16:55:50 +0100

    Rudimentary implementation of MonoSerialPort

---
+++ Solid.Arduino/MonoSerialConnection.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Solid.Arduino
+{
+    /// <summary>
+    ///     Mono specific <see cref="SerialPort">SerialPort</see> implementation
+    /// </summary>
+    public class MonoSerialConnection : ISerialConnection
+    {
+        private readonly CancellationTokenSource cancellationTokenSource;
+        private readonly SerialPort serialPort;
+        private bool isDisposed;
+        private Task serialReadTask;
+        private readonly MemoryStream internalBuffer;
+
+        public MonoSerialConnection(string portName, SerialBaudRate baudRate)
+        {
+            PortName = portName;
+            BaudRate = (int)baudRate;
+
+            cancellationTokenSource = new CancellationTokenSource();
+            serialPort = new SerialPort(portName, (int)baudRate);
+
+            internalBuffer = new MemoryStream();
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            isDisposed = true;
+
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        public event SerialDataReceivedEventHandler DataReceived;
+        public int BaudRate { get; set; }
+        public string PortName { get; set; }
+        public bool IsOpen { get; }
+        public string NewLine { get; set; }
+        public int BytesToRead { get; }
+
+        public void Open()
+        {
+            serialPort.Open();
+
+            serialReadTask = new TaskFactory(cancellationTokenSource.Token).StartNew(SerialRead, cancellationTokenSource.Token);
+        }
+
+        private async Task SerialRead()
+        {
+            var buffer = new byte[32];
+            while (true)
+            {
+                var bytesRead = await serialPort.BaseStream.ReadAsync(buffer, 0, buffer.Length, cancellationTokenSource.Token);
+
+                if (bytesRead > 0)
+                {
+                    foreach(var b in buffer.Take(bytesRead))
+                    {
+                        internalBuffer.WriteByte(b);
+                    }
+
+                    RaiseBytesRead();
+                }
+            }
+        }
+
+        private void RaiseBytesRead()
+        {
+            DataReceived?.Invoke(this, null);
+        }
+
+        public void Close()
+        {
+            cancellationTokenSource.Cancel();
+
+            serialReadTask.Wait(1500);
+
+            Dispose();
+        }
+
+        public int ReadByte()
+        {
+            return internalBuffer.ReadByte();
+        }
+
+        public void Write(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
